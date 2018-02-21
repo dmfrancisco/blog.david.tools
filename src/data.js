e1d2c6c05d878432d311df9e6d8a9c67aa1330ab
David Francisco <dmfrancisc@gmail.com>
2018-02-21 00:39:15 +0000

    Add old blog posts about create-react-video

--- src/data.js
+++ src/data.js
@@ -40,8 +40,27 @@ export default {
       date: "2018-02-18",
       gist: "f4f5311ad2ec25147bc458d791fdaeb5",
     },
+    "crv-custom-fonts": {
+      title: "CRV: Using custom fonts",
+      summary: "Using custom fonts in Create React Video is easy!",
+      date: "2017-09-02",
+      gist: "37e5dcecf5dabeed34bdd3c241ef504d",
+    },
+    "create-react-video-v020": {
+      title: "Create React Video v0.2.0 🎉",
+      summary: "The first public release of Create React Video is now available!",
+      date: "2017-08-31",
+      gist: "5c0f880ee04a884fd354019390ec9c27",
+    },
+    "create-react-video": {
+      title: "Create React Video 🎥",
+      summary: "I'm excited to share with you a side project I've been working on.",
+      date: "2017-08-27",
+      gist: "aa0ef8f07dc4b1b2bb2e9160002606f8",
+      convert: true,
+    },
     "hello-world": {
-      title: "Hello, World!",
+      title: "Hello, World! 👀",
       summary: "Hello! And welcome to this blog. How are you today?",
       date: "2017-08-16",
       gist: "8a882e5dc6d8663e03451d7dfc9a9ecd",
